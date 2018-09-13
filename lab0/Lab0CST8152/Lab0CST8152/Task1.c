aec15a04c6965e331f2ce6dcd3537f7369111a4f
YiqingC <yiqing.chen@hotmail.com>
2018-09-13 11:36:13 -0400

    all file about lab0

---
+++ lab0/Lab0CST8152/Lab0CST8152/Task1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main() {
+
+	printf("The size of type char is: %u\n", sizeof(char));
+	printf("The size of type int is: %u\n", sizeof(int));
+	printf("The size of type short int is: %u\n", sizeof(short int));
+	printf("The size of type long is: %u\n", sizeof(long));
+	printf("The size of type float is: %u\n", sizeof(float));
+	printf("The size of type double is: %u\n", sizeof(double));
+	printf("The size of type long double is: %u\n", sizeof(long double));
+
+	printf("The size of type size_t is: %u\n", sizeof(size_t));
+	printf("The size of type wchar_t is: %u\n", sizeof( wchar_t));
+	printf("The size of type unsigned char is: %u\n", sizeof(unsigned char));
+	printf("The size of type unsigned int is: %u\n", sizeof(unsigned int));
+	printf("The size of type unsigned short int is: %u\n", sizeof(unsigned short int));
+	printf("The size of type unsigned long is: %u\n", sizeof(unsigned long));
+
+
+
+
+
+
+}
